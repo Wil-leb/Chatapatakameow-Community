127f171931244e029a59cfd068dd95a7ee26b79c
Wil-leb <85292811+Wil-leb@users.noreply.github.com>
2022-02-09 15:52:25 +0100

    2022-02-09 update
    - Cleaning of the JS files (deletion of useless semicolons, harmonisation of the type of quotes used, replacing lets by consts when relevant)
    - Changes in the HTML structure of the albums.php view
    - Changes in the CSS layout of the comment and answer content and buttons for the albums.php view

---
+++ OnkeyupEvent.js
@@ -0,0 +1,76 @@
+export default class OnkeyupEvent {
+    
+//*****Title and description length*****//
+    lengthMessages() {
+        const title = document.getElementsByClassName("album-title")
+        const description = document.getElementsByClassName("album-description")
+
+        for(let i = 0; i < title.length; i ++) {
+            if(title[i]) {
+                title[i].addEventListener("keyup", () => {
+                    let titleCount = 30 - title[i].value.length
+                    const titleDiv = title[i].nextElementSibling
+
+                    if(titleDiv) {
+                        if(title[i].value) {
+                            titleDiv.textContent = `${titleCount} caractères restants`
+
+                            if(titleCount > 0 && titleCount < 30 || titleCount == 0) {
+                                titleDiv.style.color = "green"
+
+                                if(titleCount <= 1) {
+                                    titleDiv.textContent = `${titleCount} caractère restant`
+                                }
+                            }
+
+                            else if(titleCount < 0) {
+                                titleDiv.textContent = "Le titre ne doit pas dépasser 30 caractères, espaces comprises."
+                                titleDiv.style.color = "red"
+                            }
+                        }
+
+                        else {
+                            titleDiv.style.color = "inherit"
+                            titleDiv.textContent = "30 caractères restants"
+                        }
+                    }
+                });
+            }
+        }
+
+        for(let i = 0; i < description.length; i ++) {
+            if(description[i]) {
+                description[i].addEventListener("keyup", () => {
+                    let descrCount = 200 - description[i].value.length
+                    const descrDiv = description[i].nextElementSibling
+
+                    if(descrDiv) {
+                        if(description[i].value) {
+                            descrDiv.textContent = `${descrCount} caractères restants`
+
+                            if(descrCount > 0 && descrCount < 200 || descrCount == 0) {
+                                descrDiv.style.color = "green"
+
+                                if(descrCount <= 1) {
+                                    descrDiv.textContent = `${descrCount} caractère restant`
+                                }
+                            }
+
+                            else if(descrCount < 0) {
+                                descrDiv.textContent = "La description ne doit pas dépasser 200 caractères, espaces comprises."
+                                descrDiv.style.color = "red"
+                            }
+                        }
+
+                        else {
+                            descrDiv.style.color = "inherit"
+                            descrDiv.textContent = "200 caractères restants"
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+//*****END OF THE CLASS*****//
+}
