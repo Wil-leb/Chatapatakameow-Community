3085d2f4ef12c4a0de46bbf1719486de0600ea7d
Wil-leb <85292811+Wil-leb@users.noreply.github.com>
2022-02-10 21:14:02 +0100

    2022-02-10 update
    Deleting an unnecessary function in accordance with the changes in the OnclickEvent.js class

--- site_cl/assets/js/main.js
+++ site_cl/assets/js/main.js
@@ -25,7 +25,6 @@ document.addEventListener("DOMContentLoaded", () => {
     
     OnchangeEventClass.pictureMessages()
 
-    OnclickEventClass.displayForm()
     OnclickEventClass.displayAnswers()
     OnclickEventClass.openDialog()
 
