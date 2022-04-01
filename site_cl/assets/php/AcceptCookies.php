feec04e035a9a650d1f81b12e6a48b14765d8336
Wil-leb <85292811+Wil-leb@users.noreply.github.com>
2022-04-01 16:44:09 +0200

    2022-04-01 PHP addition
    Creating a PHP file for cookie acceptation

---
+++ site_cl/assets/php/AcceptCookies.php
@@ -0,0 +1,11 @@
+<?php
+setCookie("accept_cookie", true, time() + 365*24*3600, "/", null, false, true);
+
+if(isset($_SERVER["HTTP_REFERER"]) && !empty($_SERVER["HTTP_REFERER"])) {
+    header("Location:".$_SERVER["HTTP_REFERER"]);
+}
+
+else {
+    header("Location: index.php?p=home");
+}
+?>
