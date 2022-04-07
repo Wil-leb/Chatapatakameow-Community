d56a61b1ddbdf381a0ea5132b0f9dfa89478e969
Wil-leb <85292811+Wil-leb@users.noreply.github.com>
2022-04-07 16:35:21 +0200

    2022-04-07 template update
    Adding some HTML content and some JQuery scripts in the template, for a prototype AJAX notification system

--- site_cl/template/template.php
+++ site_cl/template/template.php
@@ -35,13 +35,22 @@
                         <li><a href="index.php?p=login" <?= $https::active("login") ?>>Se connecter</a></li>
 
                     <?php elseif($session::online() && !$session::admin()) : ?>
-                        <li><a href="index.php?p=account" <?= $https::active("account") ?>><i class="fas fa-user-alt"></i>Mon compte</a></li>
+                        <li><a href="index.php?p=account" <?= $https::active("account") ?>><i class="fas fa-user-alt"></i><?= $_SESSION["user"]["login"] ?></a></li>
                         
                     <?php elseif($session::online() && $session::admin()) : ?>
-                        <li><a href="index.php?p=dashboard" <?= $https::active("dashboard") ?>><i class="fas fa-user-lock"></i>Tableau de bord</a></li>
+                        <li><a href="index.php?p=dashboard" <?= $https::active("dashboard") ?>><i class="fas fa-user-lock"></i>Page admin</a></li>
                     <?php endif; ?>
 
                     <?php if($session::online()) : ?>
+                        <li>
+                            <button id="hide-content" class="notif" value="ON"><i class="fa-solid fa-bell"></i>Notifications <span class="count"></span></button>
+                            <dialog open>
+                                <div id="notif-content"></div>
+
+                                <button id="close"><i class="far fa-window-close"></i>Fermer</button>
+                            </dialog>
+                        </li>
+
                         <li><a href="index.php?p=logout" <?= $https::active("logout") ?>>Se déconnecter</a></li>
                     
                         <?php if($session::member() || $session::admin()) { ?>
@@ -93,8 +102,70 @@
 	<?php endif; ?>
 	</footer>
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<script src="./assets/js/function.js"></script>
 	<script type="module" src="./assets/js/main.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            function load_unseen_notification(view = "") {
+
+// // Mettre à jour la vue avec les notifications en utilisant ajaxfunction load_unseen_notification(view = ''){
+                $.ajax({
+                    url: "./assets/php/FetchComments.php",
+                    method: "POST",
+                    data: {view: view},
+                    dataType: "json",
+                    success:function(data) {
+                        $("#notif-content").html(data.notification);
+
+                        if(data.unseen_notification > 0) {
+                            $(".count").html("(" + data.unseen_notification + ")");
+                        }
+
+                        else {
+                            $("#notif-content").html('<p class="no-content">Aucune notification pour le moment</p>')
+                        }
+                    }
+                });
+            }
+ 
+            load_unseen_notification();
+
+// Soumettre le formulaire et obtenir de nouveaux enregistrements
+//             // $(".comment-form").on("submit", function(event) {
+//             //     event.preventDefault();
+
+//             //     if($(".email").val() != "" && $(".commentLogin").val() != "" && $("#comment").val() != "") {
+//             //         let form_data = $(this).serialize();
+
+//             //         $.ajax({
+//             //             url: "./controller/CommentFormController.php",
+//             //             method: "POST",
+//             //             data: form_data,
+//             //             success: function(data){
+//             //                 $("#comment_form")[0].reset();
+//             //                 load_unseen_notification();
+//             //             }
+//             //         });
+//             //     }
+                
+//             //     else {
+//             //         alert("Veuilles remplir tous les champs");
+//             //     }
+//             // });
+
+// // Chargement des nouvelles notifications
+            $(document).on("click", "#delete-notifications", function() {
+                $(".count").html("");
+                load_unseen_notification("yes");
+            });
+
+            setInterval(function() {
+                load_unseen_notification();
+            },  5000);
+        });
+    </script>
 </body>
 
 </html>
