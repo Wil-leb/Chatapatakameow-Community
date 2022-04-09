49b4d93e5cb0c311dbadf535ff500babc08f8799
Wil-leb <85292811+Wil-leb@users.noreply.github.com>
2022-04-09 21:26:24 +0200

    2022-04-09 PHP asset update
    Updating column names in some requests, in accordance with changes in the database

--- site_cl/assets/php/FetchComments.php
+++ site_cl/assets/php/FetchComments.php
@@ -10,12 +10,12 @@ $pdo->exec("SET NAMES UTF8");
 
 if(isset($_POST['view'])) {
     if($_POST["view"] !=  "") {
-        $update_query = $pdo->prepare("UPDATE `comments` SET `comment_notification` = ? WHERE `comment_notification` = ?");
+        $update_query = $pdo->prepare("UPDATE `comments` SET `notification_read` = ? WHERE `notification_read` = ?");
         $update_query->execute([1, 0]);
     }
 
-    $query = $pdo->prepare("SELECT `publisher_id`, `album_title`, `comment` FROM `comments`
-                            WHERE `publisher_id` = :publisherId
+    $query = $pdo->prepare("SELECT `album_author_id`, `album_title`, `comment` FROM `comments`
+                            WHERE `album_author_id` = :publisherId
                             ORDER BY `id` DESC LIMIT 5");
     $query->execute([":publisherId" => $_SESSION["user"]["id"]]);
     $output = "";
@@ -37,7 +37,7 @@ if(isset($_POST['view'])) {
     //     $output = '<p class="no-content">Aucune notification pour le moment</p>';
     // }
 
-    $status_query = $pdo->prepare("SELECT * FROM `comments` WHERE `comment_notification` = ? GROUP BY `publisher_id`");
+    $status_query = $pdo->prepare("SELECT * FROM `comments` WHERE `notification_read` = ? GROUP BY `album_author_id`");
     $status_query->execute([0]);
     $count = $status_query->RowCount();
     $data = ["notification" => $output, "unseen_notification" => $count];
