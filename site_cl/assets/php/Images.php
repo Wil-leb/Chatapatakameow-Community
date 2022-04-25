a37f68e5f72994191d04597b35b9fb38fb3d56d2
Wil-leb <85292811+Wil-leb@users.noreply.github.com>
2022-04-25 15:48:12 +0200

    Add files via upload

--- site_cl/assets/php/Images.php
+++ site_cl/assets/php/Images.php
@@ -6,24 +6,26 @@ $pdo = new PDO("mysql:host=127.0.0.1;dbname=willeb_cl","root","");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->exec("SET NAMES UTF8");
     
-$query = $pdo->prepare("SELECT `picture_name` FROM `album_pictures` WHERE `album_id` = :albumId");
+$query = $pdo->prepare("SELECT `thumbnail_name` FROM `album_thumbnails` WHERE `album_id` = :albumId");
 
 $query->execute([":albumId" => $_GET["albumId"]]);
 
 $pictures = $query->fetchAll();
 
-foreach($pictures as $picture) {
-    $picFolder = "C:/xampp/secure/albums_cl/pictures/original/";
+foreach($thumbnails as $thumbnail) {
+    $thumbFolder = "C:/xampp/secure/albums_cl/pictures/min/";
 
-    $truePicName = basename($picture["picture_name"]);
+    $trueThumbName = basename($thumbnail["thumbnail_name"]);
     
-    $picPath = $picFolder.$truePicName;
+    $thumbPath = $thumbFolder.$trueThumbName;
 
-    $data = file_get_contents($picPath);
+    $data = file_get_contents($thumbPath);
 
-    $picExtension = pathinfo($truePicName, PATHINFO_EXTENSION);
+    $thumbExtension = pathinfo($trueThumbName, PATHINFO_EXTENSION);
 
-    $base64 ='data:image/'.$picExtension.';base64,'.base64_encode($data);
+    $base64 ='data:image/'.$thumbExtension.';base64,'.base64_encode($data);
 
     echo '<img src="'.$base64.'" id="slide">';
+
+    // echo '<img src="'.$base64.'" id="slide" height="200" width="200">';
 }
